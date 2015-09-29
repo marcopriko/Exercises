a4c2a84a8f7790c8507ffd4b4e61d8ad90ad58cb
marco <marco@192.168.0.13>
2015-09-30 00:20:16 +0200

    Created Primes class and first simple algorithms to find prime numbers.

---
+++ src/com/example/sorting/Primes.java
@@ -0,0 +1,34 @@
+package com.example.sorting;
+
+public class Primes {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=100;++i){
+			if (primeBetter(i))
+				System.out.println(i+" is prime");
+		}
+
+	}
+
+	public static boolean primeNaive(int n){
+		if(n<2)
+			return false;
+		for(int i=2; i<n; i++){
+			if(n%i==0) return false;
+		}
+
+
+		return true;
+	}
+
+	public static boolean primeBetter(int n){
+		if(n<2)
+			return false;
+		int sqrt = (int) Math.sqrt(n);
+		for(int i=2; i<=sqrt; i++){
+			if(n%i==0) return false;
+		}	 
+		return true;
+	}
+
+}
